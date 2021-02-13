0c8a0c8311104fd10fa193e471f414b8e0f2bc98
Sergey <44704529+Sereggan@users.noreply.github.com>
2021-02-13 12:57:56 +0300

    Initial commit

---
+++ article-inventory/src/main/java/com/nikolaychuks/articleinventory/ArticleInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.nikolaychuks.articleinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleInventoryApplication.class, args);
+    }
+
+}
---
+++ article-inventory/src/test/java/com/nikolaychuks/articleinventory/ArticleInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolaychuks.articleinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
