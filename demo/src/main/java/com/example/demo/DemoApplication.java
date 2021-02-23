41757446ebcc1afbdd2d5095a322859a0d46a136
Sereggan <sergei.ni@yandex.ru>
2021-02-24 01:26:40 +0300

    added gateway

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.nikolaychuks.gateway;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class GatewayApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
